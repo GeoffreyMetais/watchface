d3d6c5fdd61e850f186ce87271dd0f56e4d5269c
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-05-02 22:35:27 +0200

    Add date

--- src/watchface.c
+++ src/watchface.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 
 static void main_window_load(Window *window) {
   // Get information about the Window
@@ -10,18 +11,26 @@ static void main_window_load(Window *window) {
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+      GRect(0, PBL_IF_ROUND_ELSE(48, 42), bounds.size.w, 50));
+  s_date_layer = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(98, 92), bounds.size.w, 30));
 
   // Improve the layout to be more like a watchface
   window_set_background_color(window, GColorBlack);
   text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_text(s_date_layer, "Lundi 02 Mai");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -38,9 +47,13 @@ static void update_time(struct tm *tick_time) {
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
-
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
+
+  static char s_date_buffer[20];
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %e %h", tick_time);
+  // Display this time on the TextLayer
+  text_layer_set_text(s_date_layer, s_date_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -48,6 +61,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void init(void) {
+  setlocale(LC_ALL, "");
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
